df0b686c7faab7ed283ceec579a1c5713fd19ba8
Jasper John Cinatad <slytherin9090@gmail.com>
2018-08-11 01:38:08 +0800

    bruh

--- test/index.js
+++ test/index.js
@@ -1,10 +1,1 @@
-const { expect } = require('chai')
 
-const lib = require('../lib')
-
-describe('My package', function () {
-  it('should pass', function () {
-    const answer = lib()
-    expect(answer).to.equals(42)
-  })
-})
